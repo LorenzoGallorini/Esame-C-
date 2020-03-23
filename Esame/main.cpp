b328a50f25ff1f3b03f10e8bf188f7dba50b2b53
Lorenzo Gallorini <lorenzo.gallorini@gmail.com>
2020-03-23 16:26:28 +0100

    Initial Commit

---
+++ Esame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Esame
+//
+//  Created by Lorenzo Gallorini on 23/03/2020.
+//  Copyright © 2020 Lorenzo Gallorini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
